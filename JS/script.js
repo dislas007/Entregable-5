79527acd3687e02038ed7b1b56d34a600cb85fd4
dislas007 <d.islas007@ebc.edu.mx>
2026-05-11 11:32:05 -0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+function toggleComments(id) {
+  const elem = document.getElementById(id);
+  elem.style.display = elem.style.display === "block" ? "none" : "block";
+}
+
+function likeFunction(button) {
+  button.style.fontWeight = "bold";
+  button.innerHTML = "✓ Like";
+}
+
+function toggleModal(id) {
+  const modal = document.getElementById(id);
+  modal.style.display = modal.style.display === "flex" ? "none" : "flex";
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+// ====================
+// Carrusel automático
+// ====================
+let currentSlide = 0;
+const slides = document.querySelectorAll(".hero-carousel .slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) slide.classList.add("active");
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Cambia cada 5 segundos, SI DESEAN CAMBIAR EL TIEMPO, MODIFIQUEN EL 5000 (5 SEGUNDOS)//
+setInterval(nextSlide, 5000);
+
+// Muestra la primera slide al cargar
+showSlide(currentSlide);
